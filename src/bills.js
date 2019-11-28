03a09ffa720da4a6e6904067566acf21ca21a90e
Jorge Aguiar Martin <jotamusik@gmail.com>
2019-11-28 16:22:57 +0000

    Initial commit.
    Defining function name and definition test

---
+++ src/bills.js
@@ -0,0 +1,9 @@
+
+function billsFilter() {
+
+}
+
+export {
+  billsFilter
+}
+
---
+++ test/bills.test.js
@@ -0,0 +1,8 @@
+
+import { billsFilter } from "../src/bills";
+
+describe('Example Class Definition', function () {
+  test('Should have defined a exampleVariable member', function () {
+    expect(billsFilter).toBeDefined()
+  });
+});
