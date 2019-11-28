cbaa996b372ffdab988015e054b7669f53c986fe
Jorge Aguiar Martin <jotamusik@gmail.com>
2019-11-28 17:13:03 +0000

    New Test Create a file with suffix on call

--- test/bills.test.js
+++ test/bills.test.js
@@ -1,8 +1,13 @@
 
 import { billsFilter } from "../src/bills";
+import * as fs from 'fs';
 
-describe('Example Class Definition', function () {
-  test('Should have defined a exampleVariable member', function () {
+describe('Bills Filter', function () {
+  test('Should exists the function', function () {
     expect(billsFilter).toBeDefined()
+  });
+  test('Should create a file with a suffix on the same location as input', function () {
+    billsFilter('./test/csvfile.csv');
+    expect(fs.accessSync('./test/csvfile-filtered.csv')).toBeTruthy();
   });
 });
