f2620a432a291e4f75bad2d01b9b53f2f0439178
Jorge Aguiar <jotamusik@gmail.com>
2019-11-29 11:31:56 +0000

    Refactor: Deleted unnecessary test

--- test/bills.test.js
+++ test/bills.test.js
@@ -3,9 +3,6 @@ import { billsFilter } from "../src/bills";
 import * as fs from 'fs';
 
 describe('Bills Filter', function () {
-  test('Should exists the function', function () {
-    expect(billsFilter).toBeDefined()
-  });
   test('Should create a file with a suffix on the same location as input', function () {
     billsFilter('./test/csvfile.csv');
     expect(fs.existsSync('./test/csvfile-filtered.csv')).toBeTruthy();
