c778bb11e637da090beb483cd7b69ce241f92c3b
Jorge Aguiar <jotamusik@gmail.com>
2019-11-29 11:28:51 +0000

    Green: Create a file

--- src/bills.js
+++ src/bills.js
@@ -1,6 +1,10 @@
+import * as fs from 'fs';
 
-function billsFilter() {
-
+function billsFilter(uri) {
+  let splittedUri = uri.split('/');
+  splittedUri.splice(splittedUri.length - 1, 1);
+  let path = splittedUri.join('/');
+  fs.writeFileSync(`${path}/csvfile-filtered.csv`, '');
 }
 
 export {
