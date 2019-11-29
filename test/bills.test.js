703eb330ee6c9200c2240435a61a92050b9a5a2b
Jorge Aguiar <jotamusik@gmail.com>
2019-11-29 16:53:12 +0000

    Red: Should discard IVA + IGIC setted rows

--- test/bills.test.js
+++ test/bills.test.js
@@ -7,4 +7,11 @@ describe('Bills Filter', function () {
     billsFilter('./test/csvfile.csv');
     expect(fs.readFileSync('./test/csvfile-filtered.csv').toString()).toBe(fs.readFileSync('./test/csvfile.csv').toString());
   });
+  test('Should filter those rows with IVA and IGIC (both) setted', function () {
+    let expectedOutput = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente\n" +
+        "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,\n" +
+        "2,03/08/2019,2000,2000,,8,MacBook Pro,,78544372A";
+    billsFilter('./test/csvfile.csv');
+    expect(fs.readFileSync('./test/csvfile-filtered.csv').toString()).toBe(expectedOutput);
+  });
 });
