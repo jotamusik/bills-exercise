fbb5d0d7b829e1b3a12b8216011787ce15c5db99
Jorge Aguiar <jotamusik@gmail.com>
2019-12-03 16:09:07 +0000

    Refactor: Changed the test to write into the input file.

--- test/bills.test.js
+++ test/bills.test.js
@@ -4,10 +4,22 @@ import * as fs from 'fs';
 
 describe('Bills Filter', function () {
   test('Should filter those rows with IVA and IGIC (both) setted', async function () {
+
+    let input = "Num_factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente\n" +
+        "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,\n" +
+        "2,03/08/2019,2000,2000,,8,MacBook Pro,,78544372A\n" +
+        "3,03/12/2019,1000,2000,19,8,Lenovo Laptop,,78544372A";
+
     let expectedOutput = "Num_factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente\n" +
         "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,\n" +
         "2,03/08/2019,2000,2000,,8,MacBook Pro,,78544372A";
-    await billsFilter('./test/csvfile.csv');
-    expect(fs.readFileSync('./test/csvfile-filtered.csv').toString()).toBe(expectedOutput);
+
+    let inputFile = './test/csvfile.csv';
+    let outputFile = './test/csvfile-filtered.csv';
+
+    fs.writeFileSync(inputFile, input);
+
+    await billsFilter(inputFile);
+    expect(fs.readFileSync(outputFile).toString()).toBe(expectedOutput);
   });
 });
