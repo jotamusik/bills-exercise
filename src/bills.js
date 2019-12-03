64d688d59ab17f94bf42e72068a104dd3ef42664
Jorge Aguiar <jotamusik@gmail.com>
2019-12-03 12:47:03 +0000

    Refactor: New way to read/write csv files

--- src/bills.js
+++ src/bills.js
@@ -1,18 +1,42 @@
 import * as fs from 'fs';
+import csv from 'csvtojson';
+import { parse } from 'json2csv';
 
-function getPathFrom( uri ) {
+class CsvParser {
+  readFromFile(uri) {
+    return csv().fromFile(uri);
+  }
+
+  writeToFile(data, file) {
+    let fields = [ "Num_factura", "Fecha", "Bruto", "Neto", "IVA", "IGIC", "Concepto", "CIF_cliente", "NIF_cliente" ];
+    const csvString = parse(data, { fields, quote: '' });
+    fs.writeFileSync(file, csvString);
+  }
+}
+
+
+function getPathFrom(uri) {
   let splittedUri = uri.split('/');
   splittedUri.splice(splittedUri.length - 1, 1);
   return splittedUri.join('/');
 }
 
 function billsFilter(uri) {
-  let input = fs.readFileSync(uri);
-  let outputPath = getPathFrom(uri);
-  fs.writeFileSync(`${outputPath}/csvfile-filtered.csv`, input);
+
+  return new Promise((resolve, reject) => {
+    const csvParser = new CsvParser();
+    csvParser.readFromFile(uri).then(input => {
+      let outputPath = getPathFrom(uri);
+      csvParser.writeToFile(input, `${ outputPath }/csvfile-filtered.csv`);
+      resolve();
+    });
+  });
+
+  // let input = fs.readFileSync(uri);
+  // let outputPath = getPathFrom(uri);
+  // fs.writeFileSync(`${outputPath}/csvfile-filtered.csv`, input);
 }
 
 export {
   billsFilter
 }
-
--- test/bills.test.js
+++ test/bills.test.js
@@ -3,15 +3,15 @@ import { billsFilter } from "../src/bills";
 import * as fs from 'fs';
 
 describe('Bills Filter', function () {
-  test('Should read the file and write to the output one', function () {
-    billsFilter('./test/csvfile.csv');
+  test('Should read the file and write to the output one', async function () {
+    await billsFilter('./test/csvfile.csv');
     expect(fs.readFileSync('./test/csvfile-filtered.csv').toString()).toBe(fs.readFileSync('./test/csvfile.csv').toString());
   });
-  test('Should filter those rows with IVA and IGIC (both) setted', function () {
-    let expectedOutput = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente\n" +
+  test('Should filter those rows with IVA and IGIC (both) setted', async function () {
+    let expectedOutput = "Num_factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente\n" +
         "1,02/05/2019,1000,810,19,,ACER Laptop,B76430134,\n" +
         "2,03/08/2019,2000,2000,,8,MacBook Pro,,78544372A";
-    billsFilter('./test/csvfile.csv');
+    await billsFilter('./test/csvfile.csv');
     expect(fs.readFileSync('./test/csvfile-filtered.csv').toString()).toBe(expectedOutput);
   });
 });
